1938633e28e6350b2721d9de6897b6d0917adf05
Randolph Gordon <randolph.gordon@poplogics.com>
2021-10-01 19:47:12 +0200

    feat: first added images

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { Carousel } from './pages'
-import ExampleApp from './pages/Carousel/ExampleApp'
 import {Home} from "./pages/home";
 
 const App: React.FC = () => {
--- src/pages/Carousel/ExampleApp.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { CitiesSlider, slides } from './Example'
-
-const ExampleApp = () => {
-  return <CitiesSlider slides={slides} />
-}
-
-export default ExampleApp
